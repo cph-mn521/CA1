908e306f7db42fc77d0892841016522f4304dfe6
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-09-11 11:07:56 +0200

    Bug fixing

--- src/main/java/REST/ApplicationConfig.java
+++ src/main/java/REST/ApplicationConfig.java
@@ -20,7 +20,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(REST.CarResource.class);
         resources.add(REST.JokeResource.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
     }
--- src/main/java/REST/JokeResource.java
+++ src/main/java/REST/JokeResource.java
@@ -10,7 +10,9 @@ import Facades.JokeFacade;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -21,6 +23,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 import util.EMF_Creator;
 
@@ -32,30 +36,15 @@ import util.EMF_Creator;
 @Path("joke")
 public class JokeResource {
 
-    @Context
-    private UriInfo context;
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
-            "pu",
-            "jdbc:mysql://localhost:3307/CA1",
-            "dev",
-            "ax2",
-            EMF_Creator.Strategy.CREATE);
+                "pu",
+                "jdbc:mysql://localhost:3307/CA1",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
     private static final JokeFacade FACADE = JokeFacade.Get(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    /**
-     * Creates a new instance of GenericResource
-     */
-    public JokeResource() {
-
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String random() {
-        Joke j = FACADE.getRandom();
-        return GSON.toJson(j);
-    }
 
     @GET
     @Path("all")
@@ -71,13 +60,30 @@ public class JokeResource {
         FACADE.populate();
     }
 
-    @Path("/vote")
+    @Path("vote")
     @POST
     @Produces({MediaType.APPLICATION_JSON})
     public String getById(@QueryParam("Joke") Joke J, 
                           @QueryParam("score") int Score ) {        
         double s = FACADE.vote(J,Score);
         return "{\"msg\":\"Joken har nu den scoren:" +s+"\"}";
+    }
+    
+    
+    private ExecutorService executorService = java.util.concurrent.Executors.newCachedThreadPool();
+    @GET
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public void random(@Suspended final AsyncResponse asyncResponse) {
+        executorService.submit(new Runnable() {
+            public void run() {
+                asyncResponse.resume(doRandom());
+            }
+        });
+    }
+
+    private String doRandom() {
+        Joke j = FACADE.getRandom();
+        return GSON.toJson(j);
     }
     
     
